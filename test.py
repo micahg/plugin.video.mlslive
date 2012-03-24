72e53ea72ddd243e70d43266772a9629439548e3
Micah Galizia <micahgalizia@gmail.com>
2012-03-23 20:46:49 -0400

    more robust test

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ if len(sys.argv) == 2:
     # get the streams
     streams = my_mls.getGameStreams(sys.argv[1])
     
+    if streams == None:
+        print "No streams yet."
+        sys.exit(1)
+    
     # print each stream bitrate and 
     for k in streams.keys():
         print k + ": " + streams[k]
