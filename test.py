f6c29defcc1003c6eff7d3e06d9201c6bb5927e5
Micah Galizia <micahgalizia@gmail.com>
2013-03-02 20:20:09 -0500

    update test to use flag login info

--- test.py
+++ test.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python
 
 import mlslive, sys, time
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('-u', '--user', type='string', dest='user',
+                  help="Username for authentication")
+parser.add_option('-p', '--password', type='string', dest='password',
+                  help="Password for authentication")
+parser.add_option("-g", "--game", type='string', dest='game',
+                  help="Game to display")
+
+(options, args) = parser.parse_args()
+
+if options.user == None:
+    print "ERROR: please specify a username (call with -h for help)"
+    sys.exit(1)
+elif options.password == None:
+    print "ERROR: please specify a password (call with -h for help)"
+    sys.exit(1)
 
 my_mls = mlslive.MLSLive()
-
-if len(sys.argv) == 2:
-    
-    # get the streams
-    streams = my_mls.getGameStreams(sys.argv[1])
-    
-    if streams == None:
-        print "No streams yet."
-        sys.exit(1)
-    
-    # print each stream bitrate and 
-    for k in streams.keys():
-        print k + ": " + streams[k]
-    sys.exit(0)
-
-if not my_mls.login('', ''):
+if not my_mls.login(options.user, options.password):
     print "Unable to authenticte with MLS live. please set username and password."
     sys.exit(1)
     
+if options.game != None:
+    
+    # get the streams
+    stream = my_mls.getGameStream(options.game)
+    
+    # print the stream
+    print stream
+     
+    sys.exit(0)
+
 games = my_mls.getGames()
 
 for game in games:
